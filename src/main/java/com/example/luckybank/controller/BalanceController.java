7738079cffbbe79e098c1c7d1b4f7a5263a7b0be
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-19 23:35:13 +0300

    save+

--- src/main/java/com/example/luckybank/controller/BalanceController.java
+++ src/main/java/com/example/luckybank/controller/BalanceController.java
@@ -1,20 +1,25 @@
 package com.example.luckybank.controller;
 
+
 import com.example.luckybank.service.BalanceCalculationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @AllArgsConstructor
 public class BalanceController {
 
     private final BalanceCalculationService balanceCalculationService;
 
     @GetMapping("/total-balance")
-    public ResponseEntity<Double> getTotalBalance() {
+    public String getTotalBalance(Model model) {
         double totalBalance = balanceCalculationService.calculateTotalBalance();
-        return ResponseEntity.ok(totalBalance);
+        model.addAttribute("totalBalance", totalBalance);
+        return "total_balance"; // Имя представления
     }
 }
