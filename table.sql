2573b599fe0431474300e50b0ab47291e6a6b71c
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-20 02:18:29 +0300

    save

--- table.sql
+++ table.sql
@@ -1,16 +1,2 @@
-create table card (
-    id bigserial not null,
-    balance float(53) not null,
-    card_number varchar(255), cvv varchar(255),
-    expiration_date date, client_id bigint,
-    primary key (id)
-                  )
-
-create table client
-(id bigserial not null,
- address varchar(255),
- email varchar(255),
- last_name varchar(255),
- name varchar(255),
- primary key (id)
-)
+create table card (id bigserial not null, balance float(53) not null, card_number varchar(255), cvv varchar(255), expiration_date date, client_id bigint, primary key (id))
+create table client (id bigserial not null, address varchar(255), balance varchar(255), email varchar(255), last_name varchar(255), name varchar(255), primary key (id))
