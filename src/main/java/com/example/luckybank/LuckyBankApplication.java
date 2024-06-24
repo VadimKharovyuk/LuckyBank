e710d6ceb1d6e32841918277a246a7d86807bd11
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-24 03:54:01 +0300

    add  redis

--- src/main/java/com/example/luckybank/LuckyBankApplication.java
+++ src/main/java/com/example/luckybank/LuckyBankApplication.java
@@ -2,8 +2,10 @@ package com.example.luckybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LuckyBankApplication {
 
     public static void main(String[] args) {
