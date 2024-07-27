159c3e2c626f2a1d715b95bb68b9cd7963f09486
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-28 02:35:04 +0300

    add getWeatherByCoordinates

--- src/main/java/com/example/luckybank/WeatherApi/WeatherController.java
+++ src/main/java/com/example/luckybank/WeatherApi/WeatherController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class WeatherController {
 
@@ -19,7 +18,15 @@ public class WeatherController {
 
     @GetMapping("/weather-data")
     @ResponseBody
-    public WeatherResponse getWeather(@RequestParam String city) {
-        return weatherService.getWeather(city);
+    public WeatherResponse getWeather(@RequestParam(required = false) String city,
+                                      @RequestParam(required = false) Double lat,
+                                      @RequestParam(required = false) Double lon) {
+        if (city != null && !city.isEmpty()) {
+            return weatherService.getWeatherByCity(city);
+        } else if (lat != null && lon != null) {
+            return weatherService.getWeatherByCoordinates(lat, lon);
+        } else {
+            throw new IllegalArgumentException("City name or coordinates are required");
+        }
     }
 }
--- src/main/java/com/example/luckybank/WeatherApi/WeatherService.java
+++ src/main/java/com/example/luckybank/WeatherApi/WeatherService.java
@@ -1,28 +1,22 @@
 package com.example.luckybank.WeatherApi;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
 
-    @Value("${weather.api.key}")
-    private String apiKey;
+    private final String apiKey = "7edb7af099975cd810018d918a347f18";
+    private final String weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
 
-    @Value("${weather.api.url}")
-    private String apiUrl;
-
-    private final RestTemplate restTemplate;
-
-    public WeatherService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public WeatherResponse getWeatherByCity(String city) {
+        String url = String.format("%s?q=%s&appid=%s&units=metric", weatherUrl, city, apiKey);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, WeatherResponse.class);
     }
 
-    public WeatherResponse getWeather(String city) {
-        String url = String.format("%s?q=%s&appid=%s&units=metric", apiUrl, city, apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(url, WeatherResponse.class);
-        return response.getBody();
+    public WeatherResponse getWeatherByCoordinates(Double lat, Double lon) {
+        String url = String.format("%s?lat=%f&lon=%f&appid=%s&units=metric", weatherUrl, lat, lon, apiKey);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, WeatherResponse.class);
     }
 }
